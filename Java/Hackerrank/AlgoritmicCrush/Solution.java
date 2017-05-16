1010ea6e80d4132a1480cf1175cfbec5459158f3
tanmays <porous999@gmail.com>
2017-05-16 23:59:59 +0530

    Slight modification

---
+++ Java/Hackerrank/AlgoritmicCrush/Solution.java
@@ -0,0 +1,8 @@
+package Hackerrank.AlgoritmicCrush;
+
+/**
+ * Created by tanmays on 16/5/17.
+ * Problem definition to be found here: https://www.hackerrank.com/challenges/crush
+ */
+public class Solution {
+}
--- Java/Hackerrank/PalindromeIndex/Solution.java
+++ Java/Hackerrank/PalindromeIndex/Solution.java
@@ -5,12 +5,16 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * Created by tanmays on 13/5/17.
+ * Problem definition to be found here: https://www.hackerrank.com/challenges/palindrome-index
+ */
 public class Solution {
 
-	private static List<String> caseList;
+    private static List<String> caseList;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
 
         //long starttime = System.currentTimeMillis();
 
@@ -18,42 +22,42 @@ public class Solution {
         int n = reader.nextInt();
         reader.nextLine(); //Consuming the \n
 
-		String caseString;
-		List<String> caseList = new ArrayList<>(n);
+        String caseString;
+        List<String> caseList = new ArrayList<>(n);
 
-		for(int i = 0; i < n; i++)
-		    caseList.add(reader.nextLine());
+        for(int i = 0; i < n; i++)
+            caseList.add(reader.nextLine());
 
-		Iterator<String> myIterator = caseList.iterator();
-		while (myIterator.hasNext()) {
-			caseString = myIterator.next();
-			//System.out.print (caseString + " -- ");
-			System.out.println(palindromeIndex(caseString));
+        Iterator<String> myIterator = caseList.iterator();
+        while (myIterator.hasNext()) {
+            caseString = myIterator.next();
+            //System.out.print (caseString + " -- ");
+            System.out.println(palindromeIndex(caseString));
             //System.out.println(":" + numberOfCases[i]);
-		}
+        }
         /*long endtime = System.currentTimeMillis();
 		System.out.println((endtime - starttime)/1000);*/
-	}
-	
-	private static int checkIfPalindrome(String caseString) {
-		// TODO Auto-generated method stub
-		
-		int i;
-		int caseStringLength = caseString.length();
+    }
 
-		for (i = 0; i <= caseStringLength/2; i++){
-			if (caseString.charAt(i) != caseString.charAt(caseStringLength - i - 1)) {
-				//System.out.println(caseString + " is not palindrome!!");
-				return i;
-			}
-		}
-		//System.out.println(caseString + " is a palindrome");
-		return -1;
-	}
+    private static int checkIfPalindrome(String caseString) {
+        // TODO Auto-generated method stub
 
-	public static int palindromeIndex(String caseString) {
-		int index = -1;
-		int i;
+        int i;
+        int caseStringLength = caseString.length();
+
+        for (i = 0; i <= caseStringLength/2; i++){
+            if (caseString.charAt(i) != caseString.charAt(caseStringLength - i - 1)) {
+                //System.out.println(caseString + " is not palindrome!!");
+                return i;
+            }
+        }
+        //System.out.println(caseString + " is a palindrome");
+        return -1;
+    }
+
+    public static int palindromeIndex(String caseString) {
+        int index = -1;
+        int i;
 
         int caseStringLength = caseString.length();
         String tempCaseString = null;
@@ -61,8 +65,8 @@ public class Solution {
         index = checkIfPalindrome(caseString);
 
         if (index == -1)
-			return index;
-		else {
+            return index;
+        else {
             StringBuilder tempCaseStringBuilder = new StringBuilder(caseString);
             tempCaseStringBuilder.deleteCharAt(index);
             tempCaseString = tempCaseStringBuilder.toString();
@@ -76,7 +80,7 @@ public class Solution {
                     return caseStringLength - index - 1;
             }
         }
-		
-		return index;
-	}
+
+        return index;
+    }
 }
