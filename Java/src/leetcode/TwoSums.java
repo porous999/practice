719512936f62ebcd95d1f7327d02d8837a325306
porous999 <porous9992@gmail.com>
2020-05-10 01:35:44 +0530

    Reference to questions and answers

--- Java/src/leetcode/TwoSums.java
+++ Java/src/leetcode/TwoSums.java
@@ -5,6 +5,11 @@ import java.util.Map;
 
 /**
  * Created by tanmays on 9/5/20.
+ *
+ * Prob statement: https://leetcode.com/problems/two-sum/
+ *
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
  */
 class TwoSums {
 
@@ -17,6 +22,7 @@ class TwoSums {
         return efficientMethod(nums, target);
     }
 
+    // this method is inspired from their site
     private int[] efficientMethod(int[] nums, int target) {
 
         int[] result = new int[2];
