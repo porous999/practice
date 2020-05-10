8db572e09bf7fdc635660f3b0cba6caa42fe797d
porous999 <porous9992@gmail.com>
2020-05-10 18:11:11 +0530

    Sum two numbers, and additional test cases

---
+++ Java/src/leetcode/SumTwoNumbers.java
@@ -0,0 +1,66 @@
+package leetcode;
+
+/**
+ * Created by tanmays on 10/5/20.
+ */
+
+
+public class SumTwoNumbers {
+
+    // Definition for singly-linked list.
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        return loopedBruteSolution(l1, l2);
+    }
+
+    private static ListNode loopedBruteSolution(ListNode l1, ListNode l2) {
+
+        ListNode sumHeadNode = null;
+        ListNode traversingNode = null;
+        ListNode tempNode;
+        int sum;
+        int carry = 0;
+
+        while (l1 != null && l2 != null) {
+
+            sum = l1.val + l2.val + carry;
+            carry = sum / 10;
+            tempNode = new ListNode(sum % 10);
+
+            if (null != traversingNode) {
+                traversingNode.next = tempNode;
+                traversingNode = tempNode;
+            }
+
+            if (null == sumHeadNode) {
+                sumHeadNode = tempNode;
+                traversingNode = tempNode;
+            }
+
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+
+        if (null != l1 || null != l2) {
+            tempNode = l1 != null ? l1 : l2;
+            if (null == sumHeadNode)
+                sumHeadNode = tempNode;
+            else {
+                tempNode.val = tempNode.val + carry;
+                traversingNode.next = tempNode;
+            }
+        } else if (carry != 0) {
+            traversingNode.next = new ListNode(carry);
+        }
+
+
+        return sumHeadNode;
+    }
+}
---
+++ Java/tests/leetcode/SumTwoNumbersTest.java
@@ -0,0 +1,66 @@
+package leetcode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static leetcode.SumTwoNumbers.ListNode;
+
+/**
+ * Created by tanmays on 10/5/20.
+ */
+public class SumTwoNumbersTest {
+
+    SumTwoNumbers testSumTwoNumbers = new SumTwoNumbers();
+
+    @Test
+    public void addUnequalSizedTwoNumbers() throws Exception {
+
+        assertTrue(assertCorrectSum(1234, 12034));
+        assertTrue(assertCorrectSum(0, 1949));
+    }
+
+    @Test
+    public void addSameSizedTwoNumbers() throws Exception {
+        assertTrue(assertCorrectSum(12, 12));
+        assertTrue(assertCorrectSum(1239, 1949));
+        assertTrue(assertCorrectSum(1239, 949));
+        assertTrue(assertCorrectSum(5239, 99949));
+        assertTrue(assertCorrectSum(0, 1));
+        assertTrue(assertCorrectSum(5, 5));
+    }
+
+    private int getInt(ListNode listNode, int multiplier) {
+        if (null == listNode)
+            return 0;
+
+        if (null == listNode.next)
+            return (listNode.val * multiplier);
+        return (listNode.val * multiplier) + getInt(listNode.next, multiplier * 10);
+    }
+
+    private boolean assertCorrectSum(int num1, int num2) {
+        ListNode sumList = testSumTwoNumbers.addTwoNumbers(createNumberList(num1), createNumberList(num2));
+        System.out.printf("%d + %d => Expected: %d ; Calculated: %d\n", num1, num2, num1 + num2, getInt(sumList, 1));
+        return assertNumberList(createNumberList(num1 + num2), sumList);
+    }
+
+    private boolean assertNumberList(ListNode numberList1, ListNode numberList2) {
+        return (null == numberList1 && null == numberList2) ||
+                (numberList1.val == numberList2.val && assertNumberList(numberList1.next, numberList2.next));
+    }
+
+    @Test
+    public void testAssetNumberList() {
+        assertTrue(assertNumberList(createNumberList(1234), createNumberList(1234)));
+        assertFalse(assertNumberList(createNumberList(1234), createNumberList(12304)));
+        assertFalse(assertNumberList(createNumberList(1234), createNumberList(1034)));
+    }
+
+    private ListNode createNumberList(int i) {
+        if (i == 0)
+            return null;
+
+        return new ListNode(i % 10, createNumberList(i/10));
+    }
+
+}
