14621a9a6729bdd539cfb0d385153a3e2058f732
porous999 <porous9992@gmail.com>
2020-05-16 12:34:20 +0530

    Recursive additions and product

--- Java/src/leetcode/MultiplyStrings.java
+++ Java/src/leetcode/MultiplyStrings.java
@@ -13,6 +13,65 @@ class MultiplyStrings {
     }
 
     private static String recursiveSolution(String num1, String num2) {
-        return null;
+        if (num1.isEmpty() && num2.isEmpty())
+            return "";
+        else if (num1.isEmpty() || num2.isEmpty())
+            return num1.isEmpty()? num2: num1;
+        else
+            return multiplyRecursively(num1, num2,"", 0);
+    }
+
+    private static String multiplyRecursively(String num1, String num2, String result, int shift) {
+
+        if (shift >= num2.length())
+            return result;
+        else {
+            int num2Int = Integer.parseInt(String.valueOf(num2.charAt(shift)));
+            String prodStr = getSimpleProduct(num1, num2Int, 0);
+            if (result.isEmpty())
+                result = prodStr;
+            else {
+                for (int i = 0; i < shift; i++)
+                    prodStr = prodStr.concat("0");
+                result = getSimpleSum(result, prodStr, 0);
+            }
+            return multiplyRecursively(num1, num2, result, shift + 1);
+        }
+    }
+
+    static String getSimpleSum(String num1Str, String num2Str, int carry) {
+
+        if (num2Str.isEmpty() && num1Str.isEmpty())
+            return "";
+        else {
+
+            int num2StrLen = num2Str.length();
+            int num1StrLen = num1Str.length();
+            int num2CharNum = num2StrLen == 0? 0: Integer.parseInt(String.valueOf(num2Str.charAt(num2StrLen - 1)));
+            int num1CharNum = num1StrLen == 0? 0: Integer.parseInt(String.valueOf(num1Str.charAt(num1StrLen - 1)));
+
+            int sum = carry + num2CharNum + num1CharNum;
+            carry = sum / 10;
+
+            num1Str = num1StrLen > 0? num1Str.substring(0, num1StrLen - 1): "";
+            num2Str = num2StrLen > 0? num2Str.substring(0, num2StrLen - 1): "";
+
+            return getSimpleSum(num1Str, num2Str, carry).concat(String.valueOf(sum % 10));
+        }
+    }
+
+    static String getSimpleProduct(String num1Str, int num2, int carry) {
+
+        if (num1Str.isEmpty())
+            return carry == 0? "": String.valueOf(carry);
+        else {
+
+            int num1StrLen = num1Str.length();
+
+            int sum = carry + Integer.parseInt(String.valueOf(num1Str.charAt(num1StrLen - 1))) * num2;
+            carry = sum / 10;
+
+            return getSimpleProduct(num1Str.substring(0, num1StrLen - 1), num2, carry).concat(String.valueOf(sum % 10));
+        }
     }
 }
--- Java/tests/leetcode/MultiplyStringsTest.java
+++ Java/tests/leetcode/MultiplyStringsTest.java
@@ -1,32 +1,76 @@
 package leetcode;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
+import static leetcode.MultiplyStrings.getSimpleProduct;
+import static leetcode.MultiplyStrings.getSimpleSum;
 import static leetcode.MultiplyStrings.multiply;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by tanmays on 10/5/20.
  */
 public class MultiplyStringsTest {
+
     @Test
     public void testMultiply() throws Exception {
 
-        assertTrue(assertProduct(1, 2));
-        assertTrue(assertProduct(0, 2));
-        assertTrue(assertProduct(100, 2));
-        assertTrue(assertProduct(99999, 9999));
+        assertTrue(assertProduct(1, 2, false));
+        assertTrue(assertProduct(0, 2, false));
+        assertTrue(assertProduct(999, 9, false));
+        assertTrue(assertProduct(100, 2, false));
+        assertTrue(assertProduct(99999, 9999, false));
     }
 
-    private boolean assertProduct(int num1, int num2) {
+    @Test
+    public void testGetSimpleProduct() throws Exception {
+
+        assertTrue(assertProduct(1, 2, true));
+        assertTrue(assertProduct(0, 2, true));
+        assertTrue(assertProduct(999, 9, true));
+    }
+
+    @Test
+    public void testGetSum() {
+        assertTrue(assertSum(1, 2));
+        assertTrue(assertSum(22, 33));
+        assertTrue(assertSum(22, 3300));
+    }
+
+    private boolean assertSum(int num1, int num2) {
+        String num1Str = String.valueOf(num1);
+        String num2Str = String.valueOf(num2);
+
+        String calculatedVal = getSimpleSum(num1Str, num2Str, 0);
+        String expectedValue = String.valueOf(num1 + num2);
+        System.out.printf("%s * %s => Expected: %s ; Calculated: %s\n", num1, num2, expectedValue, calculatedVal);
+        return calculatedVal.equals(expectedValue);
+    }
+
+    private boolean assertProduct(int num1, int num2, boolean simpleProd) {
 
         String num1Str = String.valueOf(num1);
         String num2Str = String.valueOf(num2);
 
-        String calculatedVal = multiply(num1Str, num2Str);
+        String calculatedVal;
+        if (simpleProd)
+            calculatedVal = getSimpleProduct(num1Str, num2, 0);
+        else
+            calculatedVal = multiply(num1Str, num2Str);
         String expectedValue = String.valueOf(num1 * num2);
+        return assertionResult(num1Str, num2Str, expectedValue, calculatedVal);
+    }
+
+    private boolean assertionResult(String num1, String num2, String expectedValue, String calculatedVal) {
         System.out.printf("%s * %s => Expected: %s ; Calculated: %s\n", num1, num2, expectedValue, calculatedVal);
         return calculatedVal.equals(expectedValue);
+    }
+
+    @Test
+    public void testRemovalOfChar() {
+        assertEquals("abcd", "abcde".subSequence(0, 5-1));
     }
 
 }
