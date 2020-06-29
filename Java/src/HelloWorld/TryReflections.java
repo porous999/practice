3e49e50c2b6418a8d062417152e142c67a884cf6
porous999 <porous9999@gmail.com>
2020-06-29 11:09:17 +0530

    Add reflection cheatsheet

---
+++ Java/src/HelloWorld/TryReflections.java
@@ -0,0 +1,27 @@
+package HelloWorld;
+
+public class TryReflections {
+
+    private final String varStr;
+    private final int varInt;
+
+    public TryReflections() {
+        varInt = 0;
+        varStr = "Init!";
+        System.out.println("Initialized with 0 params");
+    }
+
+    public TryReflections(int varInt, String varStr) {
+        this.varInt = varInt;
+        this.varStr = varStr;
+        System.out.format("Initialized with params: %d, %s\n", varInt, varStr);
+    }
+
+    private void myPrivateMethod() {
+        System.out.format("Whoa! This private method returns values: %d, %s\n", this.varInt, this.varStr);
+    }
+
+    private void myPrivateMethodWithParams(int num) {
+        System.out.format("Whoa! This private method returns sum of %d + %d = %d\n", num, this.varInt, this.varInt + num);
+    }
+}
---
+++ Java/tests/HelloWorld/TryReflectionsTest.java
@@ -0,0 +1,62 @@
+package HelloWorld;
+
+import org.junit.Test;
+
+import java.lang.reflect.*;
+
+import static org.junit.Assert.*;
+
+public class TryReflectionsTest {
+
+    @Test
+    public void testReflections() {
+        Class<TryReflections> myReflectionsReflection = TryReflections.class;
+        TryReflections tryReflectionsObj = new TryReflections(10, "Objectification");
+
+        Constructor<?>[] myRefeflectionsContructor = myReflectionsReflection.getConstructors();
+
+        System.out.println("\nConstructors:");
+        for (Constructor<?> eachConstructor : myRefeflectionsContructor) {
+            System.out.println(eachConstructor.getName() + " " + eachConstructor.getModifiers());
+        }
+
+        System.out.println("\nMethods:");
+        Method[] myReflectedMethods = myReflectionsReflection.getMethods();
+        for (Method eachMethod : myReflectedMethods) {
+            System.out.println(eachMethod.getName() + " " + eachMethod.getModifiers());
+        }
+
+        System.out.println("\nFields:");
+        Field[] declaredFields = myReflectionsReflection.getDeclaredFields();
+        for (Field eachField : declaredFields) {
+            System.out.println(eachField.getName() + " " + eachField.getModifiers());
+        }
+
+        try {
+
+            /*
+            java.lang.IllegalArgumentException: object is not an instance of declaring class
+                at sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)
+                at sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)
+                at sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)
+                at java.lang.reflect.Method.invoke(Method.java:498)
+                at HelloWorld.TryReflectionsTest.testReflections(TryReflectionsTest.java:33)
+             */
+            /*Method privateMethod = myReflectionsReflection.getDeclaredMethod("myPrivateMethod");
+            privateMethod.setAccessible(true);
+            privateMethod.invoke(myReflectionsReflection);*/
+
+            Method privateClassMethod = TryReflections.class.getDeclaredMethod("myPrivateMethod");
+            privateClassMethod.setAccessible(true);
+            privateClassMethod.invoke(tryReflectionsObj);
+
+            Method privateClassMethodWithParams = TryReflections.class.getDeclaredMethod("myPrivateMethodWithParams", int.class);
+            privateClassMethodWithParams.setAccessible(true);
+            privateClassMethodWithParams.invoke(tryReflectionsObj, 2000);
+
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
